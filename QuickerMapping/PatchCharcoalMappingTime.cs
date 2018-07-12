64a6ab1e4374ea7cc6f1c377f2fe657d9af35396
MikeyPdog <mikeypdog911@gmail.com>
2018-07-12 21:55:46 +0100

    Add QuickerMapping mod

---
+++ QuickerMapping/PatchCharcoalMappingTime.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+
+namespace QuickerMapping
+{
+    [HarmonyPatch(typeof(CharcoalItem))]
+    [HarmonyPatch("Awake")]
+    class PatchCharcoalMappingTime
+    {
+        static void Postfix(CharcoalItem __instance)
+        {
+            __instance.m_SurveyGameMinutes = 1;
+        }
+    }
+}
