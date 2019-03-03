92e6486875dfb65c3d468f1db7b77af359b583a4
MikeyPdog <mikeypdog911@gmail.com>
2019-03-03 14:37:46 +0000

    New mod: Quicker Wood Cutting.

---
+++ QuickerWoodCutting/QuickerWoodCutting.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+
+namespace QuickerWoodCutting
+{
+    [HarmonyPatch(typeof(BreakDown))]
+    [HarmonyPatch("Start")]
+    class QuickerWoodCutting
+    {
+        static void Postfix(BreakDown __instance)
+        {
+            // Branches take 5 mins instead of 10
+            if (__instance.m_DisplayName == "Branch")
+            {
+                __instance.m_TimeCostHours = 0.0833f;
+            }
+
+            // Limbs take 30 mins base (15 mins with hatchet) instead of 90 mins base (45 mins with hatchet)
+            if (__instance.m_DisplayName.EndsWith("Limb"))
+            {
+                __instance.m_TimeCostHours = 0.5f;
+            }
+        }
+    }
+}
