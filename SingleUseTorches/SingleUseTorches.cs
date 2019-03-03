de6507c2df9886df02b376809f952a021800d838
MikeyPdog <mikeypdog911@gmail.com>
2019-03-03 12:53:40 +0000

    New mod: Single Use Torches - attempting to start a fire with a torch consumes the torch (regardless of success/failure).

---
+++ SingleUseTorches/SingleUseTorches.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+
+namespace SingleUseTorches
+{
+    [HarmonyPatch(typeof(FireStarterItem))]
+    [HarmonyPatch("Start")]
+    class SingleUseTorches
+    {
+        static void Postfix(FireStarterItem __instance)
+        {
+            if (__instance.name == "GEAR_Torch")
+            {
+                __instance.m_ConditionDegradeOnUse = 100;
+            }
+        }
+    }
+}
