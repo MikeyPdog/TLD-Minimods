96d9479f4388d0d2fbb117d1f5ead11923c56b45
MikeyPdog <mikeypdog911@gmail.com>
2019-03-03 14:54:43 +0000

    Fix QuickerWoodCutting to work with the occasional large limbs which give 4x firewood

--- QuickerWoodCutting/QuickerWoodCutting.cs
+++ QuickerWoodCutting/QuickerWoodCutting.cs
@@ -11,13 +11,13 @@ namespace QuickerWoodCutting
             // Branches take 5 mins instead of 10
             if (__instance.m_DisplayName == "Branch")
             {
-                __instance.m_TimeCostHours = 0.0833f;
+                __instance.m_TimeCostHours /= 2f;
             }
 
             // Limbs take 30 mins base (15 mins with hatchet) instead of 90 mins base (45 mins with hatchet)
             if (__instance.m_DisplayName.EndsWith("Limb"))
             {
-                __instance.m_TimeCostHours = 0.5f;
+                __instance.m_TimeCostHours /= 3f;
             }
         }
     }
