67fece256387f54c17a4db4c212fce4ffd6dcc79
MikeyPdog <mikeypdog911@gmail.com>
2019-03-09 14:53:08 +0000

    Fix SingleUseTorches

--- SingleUseTorches/SingleUseTorches.cs
+++ SingleUseTorches/SingleUseTorches.cs
@@ -11,6 +11,7 @@ namespace SingleUseTorches
             if (__instance.name == "GEAR_Torch")
             {
                 __instance.m_ConditionDegradeOnUse = 100;
+                __instance.m_ConsumeOnUse = true;
             }
         }
     }
